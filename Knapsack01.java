2979c9f3a062e961d66d6126dbd4399867ed5efd
Sourabh Jain <126811697+Jain131102@users.noreply.github.com>
2025-04-28 21:20:00 +0530

    Update Knapsack01.java

--- Knapsack01.java
+++ Knapsack01.java
@@ -2,20 +2,32 @@ import java.util.*; // for Scanner
 
 public class Knapsack01 {
     static int knapsack(int W, int val[], int wt[]) {
-        int n = val.length;
+       int n = val.length;
         int dp[][] = new int[n + 1][W + 1];
 
         for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= W; j++) {
-                if (i == 0 || j == 0) {
+                if (i == 0 || j == 0)
                     dp[i][j] = 0;
-                } else if (wt[i - 1] <= j) {
-                    dp[i][j] = Math.max(dp[i - 1][j], val[i - 1] + dp[i - 1][j - wt[i - 1]]);
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
+                else if (wt[i-1] <= j)
+                    dp[i][j] = Math.max(dp[i-1][j], val[i-1] + dp[i-1][j - wt[i-1]]);
+                else
+                    dp[i][j] = dp[i-1][j];
             }
         }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        int w = W;
+        for (int i = n; i > 0; i--) {
+            if (dp[i][w] != dp[i-1][w]) {
+                list.add(i-1);    // item i-1 picked
+                w -= wt[i-1];     // subtract weight ONLY if picked
+            }
+        }
+
+        Collections.reverse(list);
+        System.out.println("Items picked (0-based indices): " + list);
+
         return dp[n][W];
     }
 
