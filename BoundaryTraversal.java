d63b81e19a8ac1219f92cf65b2d11f6a32ba5eb8
Sourabh Jain <126811697+Jain131102@users.noreply.github.com>
2025-04-28 19:56:12 +0530

    Create BoundaryTraversal.java

---
+++ BoundaryTraversal.java
@@ -0,0 +1,79 @@
+
+// User function Template for Java
+/*
+class Node
+{
+    int data;
+    Node left, right;
+
+    public Node(int d)
+    {
+        data = d;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    
+    public void left(Node root,ArrayList<Integer> ans)
+    {
+        if(root==null || root.left==null && root.right==null)
+        return;
+        
+        ans.add(root.data);
+        if(root.left!=null)
+        {
+            left(root.left,ans);
+        }
+        else
+        {
+            left(root.right,ans);
+        }
+        
+    }
+    public void right(Node root,ArrayList<Integer> ans)
+    {
+        if(root==null || root.left==null && root.right==null)
+        return;
+        
+        
+        if(root.right!=null)
+        {
+            right(root.right,ans);
+        }
+        else
+        {
+            right(root.left,ans);
+        }
+        ans.add(root.data);
+        
+    }
+    public void leaf(Node root,ArrayList<Integer> ans)
+    {
+        if(root==null)
+        return;
+        if(root.left==null && root.right==null)
+        ans.add(root.data);
+        
+            leaf(root.left,ans);
+            leaf(root.right,ans);
+        
+    }
+    
+    ArrayList<Integer> boundaryTraversal(Node root) {
+        // code here
+        ArrayList<Integer> ans=new ArrayList<>();
+        ans.add(root.data);
+        
+        if(root.left==null && root.right==null)
+        return ans;
+        
+        left(root.left,ans);
+        leaf(root.left,ans);
+        leaf(root.right,ans);
+        right(root.right,ans);
+        
+        return ans;
+    }
+}
